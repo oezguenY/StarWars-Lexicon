cb28e9a4da6bfae2bc21ac3215c2d2daccbdec67
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-04-21 22:29:23 +0200

    Project start

---
+++ Starwars Lexicon/SelectPersonVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Starwars Lexicon
+//
+//  Created by Özgün Yildiz on 21.04.21.
+//
+
+import UIKit
+
+class SelectPersonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
