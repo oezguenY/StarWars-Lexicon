d13007a892e6096658c5e901e9fce1d9bb06ef6a
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-04-25 15:23:28 +0200

    Implemented api for getting vehicles

--- Starwars Lexicon/Controller/VehiclesVC.swift
+++ Starwars Lexicon/Controller/VehiclesVC.swift
@@ -9,23 +9,51 @@ import UIKit
 
 class VehiclesVC: UIViewController {
     
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var modelLbl: UILabel!
+    @IBOutlet weak var manufacturerLbl: UILabel!
+    @IBOutlet weak var costLbl: UILabel!
+    @IBOutlet weak var lengthLbl: UILabel!
+    @IBOutlet weak var speedLbl: UILabel!
+    @IBOutlet weak var crewLbl: UILabel!
+    @IBOutlet weak var passengersLbl: UILabel!
+    
     var person: Person!
+    var vehiclesApi = VehiclesApi()
+    var vehicles = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(person.name)
-        // Do any additional setup after loading the view.
+        vehicles = person.vehicles
+        guard let firstVehicle = vehicles.first else { return }
+        getVehicle(url: firstVehicle)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getVehicle(url: String) {
+        vehiclesApi.getVehicle(url: url) { (vehicle) in
+            if let vehicle = vehicle {
+                self.setUpView(vehicle: vehicle)
+            }
+        }
     }
-    */
+    
+    func setUpView(vehicle: Vehicle) {
+        nameLbl.text = vehicle.name
+        modelLbl.text = vehicle.model
+        manufacturerLbl.text = vehicle.manufacturer
+        costLbl.text = vehicle.cost
+        lengthLbl.text = vehicle.length
+        speedLbl.text = vehicle.speed
+        crewLbl.text = vehicle.crew
+        passengersLbl.text = vehicle.passengers
+    }
+    
+    @IBAction func previousBtnClicked(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnClicked(_ sender: Any) {
+    }
+    
+    
 
 }
---
+++ Starwars Lexicon/Services/VehiclesApi.swift
@@ -0,0 +1,38 @@
+//
+//  VehiclesApi.swift
+//  Starwars Lexicon
+//
+//  Created by Özgün Yildiz on 25.04.21.
+//
+
+import Foundation
+import Alamofire
+
+class VehiclesApi {
+    
+    func getVehicle(url: String, completion: @escaping (Vehicle?) -> ()) {
+        
+    guard let url = URL(string: url) else { return }
+        Alamofire.request(url).responseJSON { (response) in
+            if let error = response.result.error {
+                debugPrint(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            
+            guard let data = response.data else { return completion(nil) }
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let homeworld = try jsonDecoder.decode(Vehicle.self, from: data)
+                completion(homeworld)
+            } catch {
+                debugPrint(error.localizedDescription)
+                completion(nil)
+                return
+            }
+            
+        }
+    
+    }
+}
